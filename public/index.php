5efe7354a2e9a8b1e4b05b7cfce029305df5695c
Andrey Bescenniy <andxbes@gmail.com>
2023-02-16 20:14:52 +0200

    Add form and user list

--- public/index.php
+++ public/index.php
@@ -1,28 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./favicon.ico" />
     <link rel="icon" type="image/png" href="./favicon.png" />
-    <link rel="stylesheet" href="./build/css/style.css" />
+    <link rel="stylesheet" href="/build/css/style.css" />
     <title>Document</title>
 </head>
-<body>
-    <?= "Hi"  ?>
 
-    <form action="">
-        <input type="text" name="name">
-        <input type="text" name="last_name">
-        <select name="position">
-            <option value="Программист">Программист</option>
-            <option value="Программист">менеджер</option>
-            <option value="Программист">тестировщик</option>
-        </select>
-        <button>Отправить</button>
-    </form>
+<body>
+    <div class="container relative  min-h-screen m-auto my-3 sm:my-10 max-w-screen-desktop">
+        <div id="user__list">
+            <div class="px-6 lg:px-8">
+                <div class="sm:flex sm:items-center">
+                    <div class="sm:flex-auto">
+                        <h1 class="text-xl font-semibold text-gray-900">Пользователи</h1>
+                        <p class="mt-2 text-sm text-gray-700">Список всех пользователей в вашей учетной записи, включая их имя, должность</p>
+                    </div>
+                    <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                        <button type="button" class="block rounded-md bg-indigo-600 py-1.5 px-3 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add user</button>
+                    </div>
+                </div>
+                <div class="mt-8 flow-root">
+                    <div class="-my-2 -mx-6 overflow-x-auto lg:-mx-8">
+                        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" class="py-3.5 pl-6 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Имя</th>
+                                        <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">Фамилия</th>
+                                        <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">Должность</th>
+
+                                        <th scope="col" class="relative py-3.5 pl-3 pr-6 sm:pr-0">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="whitespace-nowrap py-4 pl-6 pr-3 text-sm font-medium text-gray-900 sm:pl-0">Lindsay</td>
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">Walton</td>
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">Front-end Developer</td>
+                                        <td class="relative whitespace-nowrap py-4 pl-3 pr-6 text-right text-sm font-medium sm:pr-0">
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                        </td>
+                                    </tr>
+
+                                    <!-- More people... -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <!--
+    Background backdrop, show/hide based on modal state.
+
+    Entering: "ease-out duration-300"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100"
+      To: "opacity-0"
+  -->
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <!--
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      -->
+                <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                    <form class="w-full max-w-lg mx-auto" action="" id="user_form">
+                        <div class="flex flex-wrap -mx-3 mb-6">
+                            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                    Имя
+                                </label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="first_name" type="text" placeholder="Jane">
+                                <!-- <p class="text-red-500 text-xs italic">Please fill out this field.</p> -->
+                            </div>
+                            <div class="w-full md:w-1/2 px-3">
+                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                    Фамилия
+                                </label>
+                                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Doe" name="last_name">
+                            </div>
+                        </div>
+                        <div class="flex flex-wrap -mx-3 mb-6">
+                            <div class="w-full px-3">
+                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                                    Должность
+                                </label>
+                                <div class="relative">
+                                    <select name="position" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                        <option value="Программист">Программист</option>
+                                        <option value="Менеджер">Менеджер</option>
+                                        <option value="Тестировщик">Тестировщик</option>
+                                    </select>
+                                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <script defer src="./build/js/app.js"></script>
 </body>
-</html>
 
+</html>
