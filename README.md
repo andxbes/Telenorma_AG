3413a145a212d4d7ac08f802a69c9e6295453a81
Andrey Bescenniy <andxbes@gmail.com>
2023-02-16 17:59:17 +0200

    assets

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Telenorma_AG Test
+# Telenorma_AG
 1) Install Docker Desctop 
 2) in terminal run  "docker-compose up"
 3) Go to http://localhost/
--- public/index.php
+++ public/index.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./favicon.ico" />
+    <link rel="icon" type="image/png" href="./favicon.png" />
+    <link rel="stylesheet" href="./build/css/style.css" />
     <title>Document</title>
 </head>
 <body>
     <?= "Hi"  ?>
+
+    <form action="">
+        <input type="text" name="name">
+        <input type="text" name="last_name">
+        <select name="position">
+            <option value="Программист">Программист</option>
+            <option value="Программист">менеджер</option>
+            <option value="Программист">тестировщик</option>
+        </select>
+        <button>Отправить</button>
+    </form>
+    <script defer src="./build/js/app.js"></script>
 </body>
 </html>
 
