45106e68e781c6b8ef78f7bfd38f2de6f858fb96
Andrey Bescenniy <andxbes@gmail.com>
2023-02-18 17:26:02 +0200

    goods

--- public/goods.php
+++ public/goods.php
@@ -2,14 +2,37 @@
 
 class Good
 {
-    public $name;
-    public $meta;
-// public function __toString()
-// {
-//     return implode(', ', [$this->name, $this->meta]);
-// }
+    public $name = null;
+    public $meta = null;
+    public function __toString()
+    {
+        return $this->name . (!empty($this->meta) ? ', ' . $this->meta : '');
+    }
 }
 
 header('Content-Type: application/json; charset=utf-8');
 
 $db = new PDO('mysql:host=mariadb;dbname=beta;charset=utf8', 'beta', 'beta');
+
+$query = $db->query("
+SELECT goods.name,
+	GROUP_CONCAT(
+		 CONCAT(product_meta.meta_key,', ',product_meta.meta_value)
+    ) AS meta
+FROM goods
+LEFT JOIN ( SELECT additional_goods_field_values.good_id AS good_id, additional_fields.name AS meta_key, additional_field_values.name AS meta_value
+		FROM additional_goods_field_values
+		INNER JOIN additional_fields
+		ON additional_goods_field_values.additional_field_id = additional_fields.id 
+		INNER JOIN additional_field_values
+		ON additional_goods_field_values.additional_field_value_id = additional_field_values.id )  product_meta
+ON goods.id = product_meta.good_id
+group by goods.id;
+
+        ");
+$query->setFetchMode(PDO::FETCH_CLASS, 'Good');
+$result = $query->fetchAll();
+if (!empty($result)) {
+    $result = array_map(fn($good) => $good->__toString(), $result);
+    print json_encode($result);
+}
