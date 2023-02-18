778b3db1feefdabdcd95363b83569606349e456e
Andrey Bescenniy <andxbes@gmail.com>
2023-02-18 17:28:03 +0200

    note

--- public/goods.php
+++ public/goods.php
@@ -14,6 +14,28 @@ header('Content-Type: application/json; charset=utf-8');
 
 $db = new PDO('mysql:host=mariadb;dbname=beta;charset=utf8', 'beta', 'beta');
 
+//Изначально такой запрос был, но позже обьединил в один , без создания временной таблицы
+$alt_sql = "
+CREATE VIEW product_meta AS(
+	SELECT additional_goods_field_values.good_id AS good_id, additional_fields.name AS meta_key, additional_field_values.name AS meta_value
+		FROM additional_goods_field_values
+		INNER JOIN additional_fields
+		ON additional_goods_field_values.additional_field_id = additional_fields.id 
+		INNER JOIN additional_field_values
+		ON additional_goods_field_values.additional_field_value_id = additional_field_values.id 
+);
+
+SELECT goods.name,
+	GROUP_CONCAT(
+		 CONCAT(product_meta.meta_key,', ',product_meta.meta_value)
+    ) AS meta
+FROM goods
+LEFT JOIN product_meta 
+ON goods.id = product_meta.good_id
+group by goods.id;
+";
+
+
 $query = $db->query("
 SELECT goods.name,
 	GROUP_CONCAT(
